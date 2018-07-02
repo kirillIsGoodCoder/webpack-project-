e5e6c9f0a81bf2b220c928a7ace25ef5c9643fea
kirillIsGoodCoder <36628319+kirillIsGoodCoder@users.noreply.github.com>
2018-07-02 16:10:21 +0400

    add code

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,47 @@
 var cardsElem = document.getElementsByClassName('card'); // выбрал все элементы с классом 'card'.
-console.log('count of cardsElem : '+cardsElem);
+console.log('count of cardsElem : ');
 for ( i=0; i < cardsElem.length; i++ ){
   console.log(cardsElem[i]);
-  cardsElem[i].onmouseenter = cardsElem[i].onmouseleave = handler;
+  cardsElem[i].onmouseenter = cardsElem[i].onmouseleave = cardsElem[i].onmousemove = cardsElem[i].onmouseout= handler;
 }
 
 function handler( event ) {
   var type = event.type; 
   var target = event.target;
-  var leftSide = false,
+  var trDeg = 0,
+      leftSide = false,
       rightSide = false;
 
   if (type === 'mouseout') {
-    setInterval()
+    target.style.transform = "rotate3d(0, 1, 0, 0deg)";
+    target.style.transition = '0s';
   }
   if (type === 'mousemove') {
     var targetCoords = target.getBoundingClientRect();
     var posX = event.clientX - targetCoords.x,
         posY = event.clientY - targetCoords.y;
     console.log(posX, posY);
-    var center = targetCoords.x + (240px / 2);
+
+    var center = targetCoords.x + 120;
     if (posX < center) {
       leftSide = true; 
     } else {
       rightSide = true;
     }
     
+    if ( posX < 50 ){
+      trDeg = posX / 2.5;
+    } else { 
+        trDeg = 27;
+      };
+
     if ( leftSide ){
-      card.style.transform = "rotate3d(0, 1, 0, -20deg)";
+      var lTrDeg = trDeg;
+      target.style.transform = "rotate3d(0, 1, 0, -"+lTrDeg+"deg)";
     }
-    if ( rightSide){
-      cardsElem.style.transform = "rotate3d(0, 1, 0, 20deg)";
+    if ( rightSide && trDeg > 190){
+      var rTrDeg = trDeg;
+      target.style.transform = "rotate3d(0, 1, 0, "+rTrDeg+"deg)";
     }
   }
 }
